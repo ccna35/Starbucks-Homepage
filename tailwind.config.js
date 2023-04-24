72d6296644dafe780fa522ea1be124c88d76ec56
ccna35 <shawkykhalil@gmail.com>
2023-04-24 21:12:00 +0200

    "Starbucks homepage"

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {},
+    colors: {
+      skyBlue: "#d4e9e2",
+      starGreen: "#006241",
+      starPink: "#f6bfd9",
+    },
+  },
+  plugins: [],
+};
